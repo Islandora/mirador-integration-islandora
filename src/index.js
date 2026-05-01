a15b317a945b0ce423287e74a1e7c14974d73c22
Joe Corall <joe@libops.io>
2026-05-01 13:01:29 +0000

    Add browser process shim for Vite bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './process-shim';
 import Mirador from 'mirador';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 import { miradorDownloadPlugin, miradorDownloadDialogPlugin } from 'mirador-dl-plugin';
